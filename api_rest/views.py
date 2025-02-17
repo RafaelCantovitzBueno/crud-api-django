04c460b36b6e8286743bb68cd1184b2364caff3d
Rafael Cantovitz Bueno <rafael-cbueno@hotmail.com>
2025-02-17 10:22:46 -0300

    access by URL parameters

--- api_rest/views.py
+++ api_rest/views.py
@@ -30,3 +30,21 @@ def get_by_nick(request, nick):
     if request.method == 'GET':
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+# CRUD  
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+    if request.method == 'GET':
+        try:
+            if request.GET['user']: # Check if there is a get parameter called 'user'
+                user_nickname = request.GET['user'] # Find get parameter
+                try:
+                    user = User.objects.get(pk=user_nickname) # Get the object in database
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND) # User not found
+                serializer = UserSerializer(user) # Serialize the object data into json
+                return Response(serializer.data) # Return the serialized data
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
