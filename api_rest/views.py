d9a5a04852a65ccac1d1b2b7ab3004c3bfab8fe7
Rafael Cantovitz Bueno <rafael-cbueno@hotmail.com>
2025-02-17 10:47:15 -0300

    PUT - edit by request.data

--- api_rest/views.py
+++ api_rest/views.py
@@ -57,3 +57,17 @@ def user_manager(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    # Editing data
+    if request.method == 'PUT':
+        nickname = request.data['user_nickname']
+        try:
+            updated_user = User.objects.get(pk=nickname)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        print(request.data)
+        serializer = UserSerializer(updated_user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
